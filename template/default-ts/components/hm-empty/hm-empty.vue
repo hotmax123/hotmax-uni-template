e6fc7886d51bff6b1bd294e5737effb8ae8df6b5
陈亮陈亮 <chenliang@hd123.com>
2020-10-19 15:27:01 +0800

    替换资源

---
+++ template/default-ts/components/hm-empty/hm-empty.vue
@@ -0,0 +1,72 @@
+<template>
+  <view class="empty-wrapper" :style="{ 'padding-top': marginTop }">
+    <image v-if="img.length > 0" :src="img" mode="widthFix" />
+    <text v-if="title.length > 0" class="title">{{ title }}</text>
+    <text @click="clickSubtitle" v-if="subtitle.length > 0" class="subtitle">
+      {{ subtitle }}
+    </text>
+    <view>
+      <slot name="action"></slot>
+    </view>
+  </view>
+</template>
+
+<script>
+import { defImg } from './img.js'
+export default {
+  name: 'hm-empty',
+  props: {
+    img: {
+      type: String,
+      default: defImg
+    },
+    title: {
+      type: String,
+      default: '暂无数据'
+    },
+    subtitle: {
+      type: String,
+      default: ''
+    },
+    marginTop: {
+      type: String,
+      default: '100px'
+    }
+  },
+  computed: {},
+  methods: {
+    clickSubtitle() {
+      this.$emit('titleClick')
+    }
+  }
+}
+</script>
+
+<style scoped>
+.empty-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.empty-wrapper .title {
+  height: 20px;
+  font-size: 15px;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 500;
+  color: #202124;
+  line-height: 20px;
+  margin-top: 6px;
+}
+.empty-wrapper image {
+  width: calc(50%);
+}
+.empty-wrapper .subtitle {
+  height: 20px;
+  font-size: 14px;
+  font-family: PingFangSC-Regular, PingFang SC;
+  font-weight: 400;
+  color: #a1a1a1;
+  line-height: 20px;
+  margin-top: 6px;
+}
+</style>
